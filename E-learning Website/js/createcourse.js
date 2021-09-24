7ae7d80dbd45014b2c3b75e93f0ca685aa92fe80
enculandus <aniket120301@gmail.com>
2021-09-24 07:05:12 +0530

    try 1

--- E-learning Website/js/createcourse.js
+++ E-learning Website/js/createcourse.js
@@ -23,6 +23,7 @@ firebase.auth().onAuthStateChanged((user) => {
         rtdb = firebase.database();
         auth = firebase.auth();
         storage = firebase.storage();
+        loadCourses();
     } else {
         // User is signed out
         // ...
@@ -31,17 +32,84 @@ firebase.auth().onAuthStateChanged((user) => {
 });
 /* -------------------------------------------------------------------------- */
 
-/* ------------------------Loading Available topics-------------------------- */
+/* ------------------------Loading Available Courses-------------------------- */
 
-function loadTopics() {
+async function loadCourses() {
+  rtdb.ref("/courses").once('value',(snap)=>{
+    snap.forEach((childSnap) => {
+      let dat = childSnap.val();
+      const opt = "<option name=\"" + dat.name + "\" value\"" + dat.courseID + "\">"
+      let dom = new DOMParser().parseFromString(opt,'text/html');
+      let opt_element = dom.body.firstElementChild;
+      document.getElementById('courses').append(opt_element);
+    });
+  });
+}
+
+async function loadTopics() {
+  let courseID = document.getElementById("courseIdMod").value.toString();
+  let contentList = db.collection("courses").doc(courseID).collection("content").doc("contentLinks");
+  contentList.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+        let dat = doc.data();
+        dat..forEach((element) => {
+          const opt = "<option name=\"" + element.contentTitle + "\" value\"" + element.contentName + "\">"
+          let dom = new DOMParser().parseFromString(opt,'text/html');
+          let opt_element = dom.body.firstElementChild;
+          document.getElementById('courses').append(opt_element);
+        });
+
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+  }).catch((error) => {
+    console.log("Error getting document:", error);
+  });
+}
+
+async function loadContent() {
+
+}
+/* -------------------------------------------------------------------------- */
+
+/* --------------------------Creating a new Content-------------------------- */
+
+async function createContent() {
+  let courseID = document.getElementById("courseID").value.toString();
+  let contentTitle = document.getElementById("newTitle").value.toString();
+  let mainContent = document.getElementById("newContent").value.toString();
+
+  let contentRef = rtdb.ref("courseData/" + courseID + "/courseContent").push();
+  let contentID = contentRef.key;
+
+  contentRef.set({
+    title: contentTitle,
+    content: mainContent,
+    contentUID: contentID,
+  }).then(()=>{
+    db.collection("courses").doc(courseID).collection("content").doc("contentLinks").update({
+      articlesIDs: firebase.firestore.FieldValue.arrayUnion(contentID),
+      articleTitles: firebase.firestore.FieldValue.arrayUnion(contentTitle)
+    }).then(()=>{
+      location.reload();
+    }).catch((error)=>{
+      db.collection("courses").doc(courseID).collection("content").doc("contentLinks").set({
+        articlesIDs: firebase.firestore.FieldValue.arrayUnion(contentID),
+        articleTitles: firebase.firestore.FieldValue.arrayUnion(contentTitle)
+      }).then(()=>{
+        location.reload();
+      })
+    });
+  });
 
 }
 
 /* -------------------------------------------------------------------------- */
 
-document.getElementById("fileB").addEventListener('change', uploadPic);
-
 /* --------------------------Creating a new course--------------------------- */
+document.getElementById("fileB").addEventListener('change', uploadPic);
 var file, fileXtension;
 
 async function uploadPic(ev) {
