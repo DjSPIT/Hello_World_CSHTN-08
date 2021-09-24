cbc551029d30af7fdeb7c0827dd71d5b40e6b4cc
enculandus <aniket120301@gmail.com>
2021-09-24 20:31:46 +0530

    functions deployed

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 // // Create and Deploy Your First Cloud Functions
@@ -7,15 +8,13 @@ const admin = require("firebase-admin");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-
 /* *************************** Initializations ****************************** */
 admin.initializeApp();
 /* ************************************************************************** */
-
 /* *************************** Global Constants ***************************** */
 
 const db = admin.firestore();
-// const rtdb = admin.database();
+const rtdb = admin.database();
 // const auth = admin.auth();
 
 /* ************************************************************************** */
@@ -48,6 +47,21 @@ exports.deleteProfile = functions.auth.user().onDelete((user) => {
   return profile
       .delete()
       .catch(console.error);
+});
+/* -------------------------------------------------------------------------- */
+
+/* ---------------------------vote for a link-------------------------------- */
+
+exports.vote = functions.https.onCall((data, context) =>{
+  //  making a reference
+  const str = "courseData/" + data.courseID + "/courseContent" + data.contentID;
+  const voteRef = rtdb.ref(str + data.link + "/votes");
+  //  returning a promise
+  return voteRef.once("value", (snap)=>{
+    let theInt = snap.val();
+    theInt += 1;
+    voteRef.update(theInt);
+  });
 });
 /* -------------------------------------------------------------------------- */
 
